56516dd989e6e0ec84f4a6b39081bf62954b83f2
Julian Ospald <hasufell@posteo.de>
2021-08-20 11:20:49 +0200

    Fix locale encoding in bench to fix windows CI

--- abstract-filepath/bench/Bench.hs
+++ abstract-filepath/bench/Bench.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 
+import           GHC.IO.Encoding
 import           Test.Tasty.Bench
 
 import qualified Short.BenchAll as SBA
@@ -10,5 +11,6 @@ import qualified Short.Word16.BenchAll as SWBA
 
 main :: IO ()
 main = do
+  setLocaleEncoding utf8
   defaultMain
     (SBA.benchmarks ++ SWBA.benchmarks)
